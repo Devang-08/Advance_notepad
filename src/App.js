f8c6ec1b5aab38ac686278cb7c935dd2a2a3e544
Devang-08 <devangsg08@gmail.com>
2023-12-11 10:08:20 +0530

    changed in router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import NavBar from './Components/NavBar';
 import Notepad from './Components/Notepad';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    <NavBar />
-    <Notepad />
-    </>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Notepad />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      
+    </BrowserRouter>
   );
 }
 
